d61ee85e7a3027df78e0389db19c7041744d0644
Chris <Britten63@hotmail.com>
2025-08-04 15:25:52 -0300

    Desc Added, IPO Comments Added

---
+++ main.py
@@ -0,0 +1,63 @@
+#==============================
+#=== JOHNNY-5 ROBOT PROGRAM ===
+#==============================
+# Date: 08-04-2025
+#==============================
+# Author(s): Robot Group 3 
+#==============================
+#Desc: This is a program that allows the connected robot ( JOHNNY - 5 )
+# JOHNNY-5 will perform dynamic movments throughout an obsticle course while being run via an IDE using Python.
+# Version control using Git.
+# The program will provide function to the robot and the output will be a dynamic rnage of movements
+# that allows the robot to complete a predetermed course as per requirments.
+
+
+
+#========
+# Define required libraries.
+#========
+
+
+
+#========
+# Define required constants.
+#========
+
+
+#========
+# Define required functions.
+#========
+
+
+#========
+# Gather user inputs.
+#========
+
+
+#========
+# Main program starts here.
+#========
+
+
+
+#========
+# Perform required calculations.
+#========
+    
+
+#========
+# Display results
+#========
+
+    
+#========
+ # Write the values to a data file for storage.
+#========
+
+
+#========
+# Any housekeeping duties at the end of the program.
+#========
+
+   
+
